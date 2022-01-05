bfc5a963078fa56faeb3506d8a5247aac49fbcbe
lucas hellanio <lucas.hellanio@liferay.com>
2022-01-05 01:18:26 -0300

    main | Adjust name and css global

---
+++ dev/frontend/src/pages/Dashboard/ChatBox/index.tsx
@@ -0,0 +1,65 @@
+import "./style.css";
+import { FaUser, FaBell } from "react-icons/fa";
+
+export default function ChatBox() {
+  return (
+    <div className="home-container">
+      <h1>ChatBox</h1>
+
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#adff2f" size={25} />
+          <span>Lucas Hellanio</span>
+          <strong>
+            <FaBell />
+          </strong>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#ff0000" size={25} />
+          <span>Pedro Gabriel</span>
+          <strong>
+            <FaBell />
+          </strong>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#ff0000" size={25} />
+          <span>Victor guilherme</span>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#adff2f" size={25} />
+          <span>Silas Nunes</span>
+          <strong>
+            <FaBell />
+          </strong>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#adff2f" size={25} />
+          <span>Eric Monteiro</span>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#ffff00" size={25} />
+          <span>Sara Silva</span>
+        </div>
+      </div>
+      <div className="home-content">
+        <div className="professional-qtt">
+          <FaUser color="#ffff00" size={25} />
+          <span>Rafaela Melo</span>
+          <strong>
+            <FaBell />
+          </strong>
+        </div>
+      </div>
+    </div>
+  );
+}
